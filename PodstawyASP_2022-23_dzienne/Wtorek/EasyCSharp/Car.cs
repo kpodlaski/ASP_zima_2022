854ae4a6e592ad24b5c83dd5e11961177dd42693
kpodlaski <podlaski@uni.lodz.pl>
2022-10-11 11:44:28 +0200

    11.10.2022

---
+++ PodstawyASP_2022-23_dzienne/Wtorek/EasyCSharp/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace EasyCSharp
+{
+    class Car : Vehicle, IComparable<Car>
+    {
+        public String PlateNumber { private set; get; }
+        public Car(string plateNumber, string Owner, int productionYear) 
+            : base(Owner, productionYear)
+        {
+            this.PlateNumber = plateNumber;
+        }
+
+        public int CompareTo([AllowNull] Car other)
+        {
+            return this.PlateNumber.CompareTo(other.PlateNumber);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Car)
+                return this.PlateNumber.Equals( ((Car) obj).PlateNumber);
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.PlateNumber.GetHashCode();
+        }
+
+        public static IComparer<Car> GetComparerByProductionYear()
+        {
+            return new ComparerByProductionYear();
+        }
+
+        private class ComparerByProductionYear : Comparer<Car>
+        {
+            public override int Compare([AllowNull] Car x, [AllowNull] Car y)
+            {
+                return x.ProductionYear-y.ProductionYear;
+            }
+        }
+
+    }
+}
---
+++ PodstawyASP_2022-23_dzienne/Wtorek/EasyCSharp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EasyCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Vehicle v = new Vehicle("Adam", 1956);
+            Console.WriteLine(v.Owner);
+            //v.Owner = "Eliza";
+            Console.WriteLine("Pojazd -- "+v);
+            v = null;
+            Car c = new Car("EL1234A","Eliza", 2018);
+            Console.WriteLine("Samochód -- " + c);
+            List<Car> cars = new List<Car>();
+            cars.Add(new Car("ELW213B", "Tamara", 1998));
+            cars.Add(c);
+            cars.Add(new Car("EL2134B", "Tomasz", 2022));
+            cars.Add(new Car("EL2134B", "Tadeusz", 2022));
+            Console.WriteLine(cars[2]);
+            Console.WriteLine("=======================");
+            for (int i = 0; i < cars.Count; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+            cars.Sort();
+            Console.WriteLine("-----------------------");
+            foreach (Car cItem in cars)
+            {
+                Console.WriteLine(cItem);
+            }
+            cars.Sort(Car.GetComparerByProductionYear());
+            Console.WriteLine("-----------------------");
+            foreach (Car cItem in cars)
+            {
+                Console.WriteLine(cItem);
+            }
+            HashSet<Car> setOfCars = new HashSet<Car>();
+            foreach (Car cItem in cars)
+            {
+                setOfCars.Add(cItem);
+            }
+            Console.Write("Rozmiar zbioru samochodów:");
+            Console.WriteLine(setOfCars.Count);
+            Console.WriteLine("+++++++++++++++++++++++");
+            foreach (Car cItem in setOfCars)
+            {
+                Console.WriteLine(cItem);
+            }
+            Dictionary<String, Car> mapOfCars = new Dictionary<string, Car>();
+            foreach (Car cItem in cars)
+            {
+                mapOfCars.Add(cItem.Owner, cItem);
+            }
+            Console.WriteLine("----------------------");
+
+            Console.WriteLine(mapOfCars["Tamara"]);
+
+
+
+
+
+
+        }
+
+    }
+}
