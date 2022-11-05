173b7277ccfddb695aa724275cf49a7cf601ce63
kpodlaski <podlaski@uni.lodz.pl>
2022-11-05 15:36:23 +0100

    odczytwww

---
+++ PodstASP_2022-23_zaoczne/StartApp/Strumienie.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace StartApp
+{
+    class Strumienie
+    {
+
+        static void Zapis()
+        {
+            StreamWriter sw = new StreamWriter("PanTadeusz.txt");
+            sw.Write("L");
+            sw.WriteLine("itwo Ojczyzno moja ty jesteś ....");
+            sw.Close();
+        }
+
+        static void Odczyt()
+        {
+            try
+            {
+                //StreamReader sr = new StreamReader("tmp\\PanTadeusz.txt_");
+                StreamReader sr = new StreamReader("PanTadeusz.txt");
+                Console.WriteLine((char) sr.Read());
+                Console.WriteLine(sr.ReadLine());
+                while (!sr.EndOfStream)
+                {
+                    Console.Write((char)sr.Read());
+                }
+                sr.Close();
+            }         
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("OJOJOJ, ERROR!!!!!");
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nFinally Done!!");
+            }
+        }
+
+        static void OdczytZWWW()
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                StreamReader sr = new StreamReader(wc.OpenRead("http://www.uni.lodz.pl"));
+                Console.WriteLine((char)sr.Read());
+                Console.WriteLine(sr.ReadLine());
+                while (!sr.EndOfStream)
+                {
+                    Console.Write((char)sr.Read());
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static public void Main(String[] a)
+        {
+            //Zapis();
+            //Odczyt();
+            OdczytZWWW();
+        }
+    }
+}
